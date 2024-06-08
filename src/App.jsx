8d115449c2d851b9b9abbd546f0f818099a3c1fc
Eden-Cohen1 <edencohen.dev@gmail.com>
2024-06-08 15:17:38 +0300

    finishing touches

--- src/App.jsx
+++ src/App.jsx
@@ -4,83 +4,109 @@ import AddProject from "./components/AddProject";
 import { useState } from "react";
 import ProjectEdit from "./components/ProjectEdit";
 
-function projecstDeepCopy(projectList){
-  return projectList.map(project =>{
+function projecstDeepCopy(projectList) {
+  return projectList.map((project) => {
     return {
       ...project,
-      tasks:[...project.tasks]
-    }
-  })
+      tasks: [...project.tasks],
+    };
+  });
 }
 
 function App() {
-  
-  const [idCount, setIdCount] = useState(0)
-  const [currentSection, setCurrentSection] = useState('no-project')
-  const [projectList, setProjectList] = useState([])
-  const [currentProjectIndex, setCurrentProjectIndex] = useState(0)
-  const openAddProject = function(){
-    setCurrentSection('add-project')
-  }
+  const [idCount, setIdCount] = useState(0);
+  const [currentSection, setCurrentSection] = useState("no-project");
+  const [projectList, setProjectList] = useState([]);
+  const [currentProjectIndex, setCurrentProjectIndex] = useState(0);
+  const openAddProject = function () {
+    setCurrentSection("add-project");
+  };
 
-  const closeAddProject = function(){
-    setCurrentSection('no-project')
-  }
-  
-  const handleAddProject = function(title, description, date){
-    setIdCount(id => ++id)
-    const newProject = {title: title, description: description, date: date, tasks: [], id:idCount}
-    const projects = projecstDeepCopy(projectList, currentProjectIndex)
-    setProjectList([...projects, newProject])
-  }
+  const closeAddProject = function () {
+    setCurrentSection("no-project");
+  };
 
-  const handleDeleteProject = function(){
-      const newProjectsArray = projectList.filter((project) => project.id !== projectList[currentProjectIndex].id)
-      setProjectList([...newProjectsArray])
-      setCurrentSection('no-project');
-  }
-  
-  const handleSelectProject = function(index){
+  const handleAddProject = function (title, description, date) {
+    setIdCount((id) => ++id);
+    const newProject = {
+      title: title,
+      description: description,
+      date: date,
+      tasks: [],
+      id: idCount,
+    };
+    const projects = projecstDeepCopy(projectList, currentProjectIndex);
+    setProjectList([...projects, newProject]);
+  };
+
+  const handleDeleteProject = function () {
+    const newProjectsArray = projectList.filter(
+      (project) => project.id !== projectList[currentProjectIndex].id
+    );
+    setProjectList([...newProjectsArray]);
+    setCurrentSection("no-project");
+  };
+
+  const handleSelectProject = function (index) {
     setCurrentProjectIndex(index);
-    setCurrentSection('project-edit')
-  }
+    setCurrentSection("project-edit");
+  };
 
-  const handleAddTask = function(task){
-    const projects = projectList.map(project =>{
-      if(project.id === projectList[currentProjectIndex].id){
+  const handleAddTask = function (task) {
+    const projects = projectList.map((project) => {
+      if (project.id === projectList[currentProjectIndex].id) {
         return {
           ...project,
-          tasks: [...project.tasks, task]
-        }
+          tasks: [...project.tasks, task],
+        };
       }
       return project;
-    })
+    });
     setProjectList(projects);
-  }
+  };
 
-    const handleDeleteTask = function(taskIndex){
-      let currentTasks = [...projectList[currentProjectIndex].tasks]
-      currentTasks.splice(taskIndex,1)
-      const projects = projectList.map(project =>{
-        return {
-          ...project,
-          tasks:[...project.tasks]
-        }
-      });
-      projects[currentProjectIndex].tasks = [...currentTasks]
-      setProjectList(projects)
-    }
+  const handleDeleteTask = function (taskIndex) {
+    let currentTasks = [...projectList[currentProjectIndex].tasks];
+    currentTasks.splice(taskIndex, 1);
+    const projects = projectList.map((project) => {
+      return {
+        ...project,
+        tasks: [...project.tasks],
+      };
+    });
+    projects[currentProjectIndex].tasks = [...currentTasks];
+    setProjectList(projects);
+  };
 
   return (
     <main className="flex h-full min-h-[100vh] pt-[5%]">
-      <Sidebar onSelectProject={handleSelectProject} onAddProjectSection={openAddProject} projectList={projectList} currentProject={projectList && projectList[currentProjectIndex]?.id}/>
-      {currentSection === 'add-project' && <section className="w-full flex items-top justify-center mb-[10%] mt-[10%]">
-      <AddProject onAddProject={handleAddProject} onCloseAddProject={closeAddProject}/>
-      </section>}
-        {currentSection === 'no-project' && <NoProjects onAddProjectSection={openAddProject}/>}
-      {currentSection === 'project-edit' && <section className="w-full flex items-top mt-[10%] mb-[10%]">
-        <ProjectEdit project={projectList && projectList[currentProjectIndex]} onAddTask={handleAddTask} onDeleteTask={handleDeleteTask} onDeleteProject={handleDeleteProject}/>
-      </section>}
+      <Sidebar
+        onSelectProject={handleSelectProject}
+        onAddProjectSection={openAddProject}
+        projectList={projectList}
+        currentProject={projectList && projectList[currentProjectIndex]?.id}
+      />
+      {currentSection === "add-project" && (
+        <section className="w-full flex items-top justify-center mb-[10%] mt-[10%]">
+          <AddProject
+            onAddProject={handleAddProject}
+            onCloseAddProject={closeAddProject}
+          />
+        </section>
+      )}
+      {currentSection === "no-project" && (
+        <NoProjects onAddProjectSection={openAddProject} />
+      )}
+      {currentSection === "project-edit" && (
+        <section className="w-full flex items-top mt-[10%] mb-[10%]">
+          <ProjectEdit
+            project={projectList && projectList[currentProjectIndex]}
+            onAddTask={handleAddTask}
+            onDeleteTask={handleDeleteTask}
+            onDeleteProject={handleDeleteProject}
+          />
+        </section>
+      )}
     </main>
   );
 }
